e714867d5b8367f5a61ac8cafa8b8ce3c617e4f2
B murali <123619161+MuraliB123@users.noreply.github.com>
2023-06-25 09:51:58 +0530

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -1,27 +1,49 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-class MyLinearRegression:
 
-    def __init__(self, learning_rate=0.001, num_iterations=1000):
-        self.learning_rate = learning_rate
-        self.num_iterations = num_iterations
-        self.weights = None
-        self.bias = None
+def grad_des(x, y):
+    m = c = 0
+    iterations = 10000
+    n = len(x)
+    best_cost = float('inf')
+    best_m = best_c = best_lr = best_iteration = 0
 
-    def train(self, X, y):
-        num_samples, num_features = X.shape
-        self.weights = np.zeros(num_features)
-        self.bias = 0
+    for lr in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]:
+        for iteration in range(iterations):
+            y_pred = m * x + c
+            q = y - y_pred
+            cost = (1 / n) * sum([val**2 for val in q])
+            dm = -(2 / n) * sum(x * q)
+            dc = -(2 / n) * sum(q)
+            m = m - lr * dm
+            c = c - lr * dc
+            if cost < best_cost:
+                best_cost = cost
+                best_m, best_c, best_lr, best_iteration = m, c, lr, iteration
 
-        for _ in range(self.num_iterations):
-            y_pred = np.dot(X, self.weights) + self.bias
+    print("Best values - m: {}, c: {}, learning rate: {}, iteration: {}, cost: {}".format(
+        best_m, best_c, best_lr, best_iteration, best_cost))
 
-            dw = (1 / num_samples) * np.dot(X.T, (y_pred - y))
-            db = (1 / num_samples) * np.sum(y_pred - y)
+  
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
+grad_des(x,y)
 
-            self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
+x = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)  
+y = np.array([5, 7, 9, 11, 13])
 
-    def predict(self, X):
-        y_pred = np.dot(X, self.weights) + self.bias
-        return y_pred
+
+model = LinearRegression()
+
+
+model.fit(x, y)
+
+
+m = model.coef_[0]
+c = model.intercept_
+
+print("Slope (m):", m)
+print("Intercept (c):", c)
+
+
